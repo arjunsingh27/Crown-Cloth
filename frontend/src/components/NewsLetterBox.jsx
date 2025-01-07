8c55e782128d22a05cdeae536af0a699e9650f10
arjunsingh27 <arjunsingh15102003@gmail.com>
2025-01-07 23:56:09 +0530

    Working on Collection Page

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
-const NewsLetterBox = () => {
+const NewsletterBox = () => {
     const onSubmitHandler = (event)=>{
-        event.preventDefa
+        event.preventDefault();
     }
+
   return (
-    <div className="text-center">
-        <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
-        <p className="text-gray-400 mt-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate eaque reprehenderit  
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subscribe Now & get 20% off</p>
+        <p className='text-gray-400 mt-3'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis perferendis vel.
         </p>
-        <form action="" className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
-            <input type="email" className='w-full sm:flex-1 outline-none '  />
-            <button className='bg-black text-white  text-xs px-10 py-4' type='submit'>Subscribe</button>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+            <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter Your Email' required />
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4'>Subscribe</button>
         </form>
     </div>
   )
 }
 
-export default NewsLetterBox
+export default NewsletterBox
