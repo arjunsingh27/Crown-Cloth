8b125a4eb57b0e92874135a6f86524788092b873
arjunsingh27 <arjunsingh15102003@gmail.com>
2025-01-20 14:12:56 +0530

    product Controller all function build and working fine with Post man addproducts ,listProducts , remove product , singleproduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -94,9 +94,30 @@ const listProducts = async (req, res) => {
  
 
 // Function for removing product
-const removeProducts = async (req, res) => { };
+const removeProducts = async (req, res) => { 
+  try {
+    await productModel.findByIdAndDelete(req.body.id)
+    res.json({success:true,message:"Product Removed with id : " + req.body.id})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:error.message});
+  }
+};
 
 // Function for single product info
-const singleProducts = async (req, res) => { };
+const singleProducts = async (req, res) => {
+
+  try {
+    const {productId} = req.body;
+    const product = await productModel.findById(productId)
+    res.json({success:true,product})
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message});
+    
+  }
+
+ };
 
 export { addProducts, listProducts, removeProducts, singleProducts };
