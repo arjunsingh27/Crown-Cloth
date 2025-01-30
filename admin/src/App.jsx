eafee798392df0e2d2d66a38eb9ecdb2875e45ee
arjunsingh27 <arjunsingh15102003@gmail.com>
2025-01-30 17:59:22 +0530

    fix import in App.jsx from Component to component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./Components/Navbar";
-import Sidebar from "./Components/Sidebar";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
