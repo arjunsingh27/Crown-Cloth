713374a9fc47c7495d43a51131bce238469d1e52
arjunsingh27 <arjunsingh15102003@gmail.com>
2025-01-30 17:39:45 +0530

    Fix import for .env

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,40 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+ 
+ 
 
-// App Config
-dotenv.config();
-const app = express();
+//App Config
+const app = express()
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-// Middle Wares
-app.get("/", (req, res) => {
-  res.send("Hello World For Vercel");
-});
+               
+
+//Middle Wares
+
 app.use(express.json());
+app.use(cors());
 
-// CORS Configuration
-app.use(cors({
-  origin: 'https://crown-cloth-frontend.vercel.app', // Allow only your frontend's domain
-  credentials: true, // If you're using cookies or other credentials
-}));
 
-// API Endpoints
-app.use("/api/user", userRouter);
-app.use("/api/products", productRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
+//Api end Points 
+app.use('/api/user',userRouter)
+app.use('/api/products', productRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/order', orderRouter)
 
-// Live Server 
-app.listen(port, () => {
-  console.log(`The server started on port ${port}`);
-});
+app.get('/',(req,res)=>{
+  res.send("API IS WORKING ");
+})
+
+
+app.listen(port,()=>{
+  console.log(`Server Started on port ${port} `);
+})
